21e7f60e64caac9fc17ba1d7f94aff088f7afe7b
Samuel Davis <samueljakdavis@gmail.com>
2017-02-21 01:54:27 -0500

    Testing BaseParameter (via ParameterTest)

---
+++ tests/Components/ParameterTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Components;
+
+use SwagBag\Components\Parameters\Parameter;
+use SwagBag\Constants\Type;
+use Tests\TestCase;
+
+class ParameterTest extends TestCase
+{
+    public function testItCompilesDefaults()
+    {
+        $expected = [
+            'name' => 'limit',
+            'in' => Parameter::QUERY,
+            'type' => Type::INTEGER,
+            'required' => false,
+        ];
+
+        $schema = new Parameter($expected['name'], $expected['in'], $expected['type'], $expected['required']);
+
+        static::assertComponentStructure($expected, $schema);
+    }
+}
+
